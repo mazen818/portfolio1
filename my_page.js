6dd045349a5216fd19e43e6664ae8dd056f7914e
Mazen <mazenalblshy@gmail.com>
2025-10-28 03:45:23 +0300

    Add files via upload

---
+++ my_page.js
@@ -0,0 +1,12 @@
+function x() {
+    document.querySelector('.nav-links').style.display = 'flex';
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('close').style.display = 'flex';
+}
+
+function y() {
+    document.querySelector('.nav-links').style.display = 'none';
+    document.getElementById('menu').style.display = 'flex';
+    document.getElementById('close').style.display = 'none';
+}
+

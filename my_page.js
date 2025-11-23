d18920d34285bb8514147f08b71b59056dc79749
Mazen <mazenalblshy@gmail.com>
2025-11-23 03:01:44 +0200

    Add files via upload

--- my_page.js
+++ my_page.js
@@ -1,3 +1,4 @@
+
 let x = true;
 
 function y() {
@@ -5,84 +6,37 @@ function y() {
     const close = document.getElementById('close'); 
     const navlinks = document.querySelector('.nav-links');
     if (x) {
-        navlinks.style.display = 'flex';
+        navlinks.style.cssText = `display: flex;
+        animation: nav-open 0.4s forwards alternate;`;
         menu.style.display = 'none';
         close.style.display = 'flex';
         x = false;
-    } else {
-        navlinks.style.display = 'none';
+    } else if (x==false) {
+        navlinks.style.cssText = `animation: nav-open 0.4s ease-in-out forwards reverse;`;
         menu.style.display = 'flex';
         close.style.display = 'none';
         x = true;
+        
     }   
 }
+//--------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------------------------------------
+
 
 window.onscroll = function() {
     let btn = document.getElementById('btn');
-    let h1 = document.querySelector('.about h1');
-    let skills = document.querySelector('.skills h1')
-    let project = document.querySelector('.my_projects h1')
-    let p = document.querySelector('.info p');
-    let list = document.querySelector('.list');
-    let education = document.querySelector('.about .education');
-
-    if (this.scrollY >= 750) {
+    if(scrollY >= 700) {
         btn.style.display = 'block';
-    } else if(this.scrollY <= 750) {
-        btn.style.cssText = `display: none;`;
+    } else {
+        btn.style.display = 'none';
     }
-    
-    if (scrollY >= 270) {
-        h1.style.cssText = `
-        display: block;
-        animation: h1 1s forwards;`;
-    } else if (scrollY <= 210) {
-        h1.style.cssText = `display: none;`;
-    }
-
-    if (scrollY >= 460) {
-        p.style.cssText = `
-        display: block;
-        animation: p 1s forwards;`;
-
-        education.style.cssText = `
-        display:block;
-        animation: education 1s;`;
-    } else if (scrollY <= 350) {
-        p.style.cssText = `display: none;`;
-        education.style.cssText = `display: none;`;
-    }
-
-    if (scrollY >= 650) {
-            list.style.cssText = `
-            display: block;
-            animation: p 1s forwards;`;
-        } else if (scrollY <= 580) {
-            list.style.cssText = `display: none;`;
-        }
-
-        if (scrollY >= 1200) {
-            skills.style.cssText = `
-            display: block;
-            animation: h1 1s forwards;`;
-        } else if (scrollY <= 1120) {
-            skills.style.cssText = `display: none;`;
-        }
-        if (scrollY >= 1750) {
-            project.style.cssText = `
-            display: block;
-            animation: h1 1s forwards;`;
-        } else if (scrollY <= 1670) {
-            project.style.cssText = `display: none;`;
-        }
 }
-
-
 
 function topfunction() {
     document.documentElement.scrollTop = 0;
 }
-
 
 onload = function() {
     let image = document.querySelector('.image');
